505134aaf11466d665c0b24c42158557b0d1a4f4
Noah Zoschke <nzoschke@gmail.com>
2015-10-06 15:14:26 -0700

    CF lookup url for papertrail drain

--- index.js
+++ index.js
@@ -1,27 +1,66 @@
+var aws = require('aws-sdk')
+var cf = new aws.CloudFormation()
+
 var winston = require('winston')
 require('winston-papertrail').Papertrail
 
-var tr = new winston.transports.Papertrail({
-    host: 'X.papertrailapp.com',
-    port: 1234,
-    hostname: 'app',
-    program: 'process',
-    showLevel: false,
-})
-var logger = new winston.Logger({ transports: [tr] })
-
 exports.handler = function(event, context) {
-  tr.on('connect', function() {
-    event.Records.forEach(function(record) {
-      var payload = new Buffer(record.kinesis.data, 'base64').toString('ascii')
+  // get app name from Kinesis record
+  var appName = "unknown"
 
-      // save on log storage by filtering out status checks
-      if (payload.lastIndexOf('kernel: ns=kernel at=request state=success status=200 method="GET" path="/check"', 0) === 0)
-        return
+  var parts = event.Records[0].eventSourceARN.split("/")
+  if (parts.length == 2) {
+    appName = parts[1]
+  }
 
-      logger.info(payload)
+  // get stack name from Lambda function name
+  // e.g. papertrail-PapertrailLogger-Y1HBW4I1TMB7 -> papertrail
+  var stackName = process.env.AWS_LAMBDA_FUNCTION_NAME.split("-")[0]
+
+  cf.describeStacks({ StackName: stackName }, function(err, data) {
+    if (err) {
+      console.log(err, err.stack)
+      return context.fail(err)
+    }
+
+    // get URL, host, port from stack parameters
+    var url = ""
+
+    var stack = data.Stacks[0]
+    for (var i = 0; i < stack.Parameters.length; i++) {
+      p = stack.Parameters[i]
+
+      if (p.ParameterKey == "Url") {
+        url = p.ParameterValue
+        break
+      }
+    }
+
+    if (!url) {
+      return context.fail(Error("stack " + stackName + " is missing Url parameter"))
+    }
+
+    // connect to Papertrail
+    var tr = new winston.transports.Papertrail({
+      host: url.split(":")[0],
+      port: url.split(":")[1],
+      hostname: appName,
+      program: 'process',
+      showLevel: false,
     })
-    tr.close()
-    context.done(null)
-  })
+
+    var logger = new winston.Logger({ transports: [tr] })
+
+    // log every kinesis record to Papertrail
+    tr.on('connect', function() {
+      event.Records.forEach(function(record) {
+        var payload = new Buffer(record.kinesis.data, 'base64').toString('ascii')
+        logger.info(payload)
+      })
+
+      tr.close()
+
+      context.done(null)
+    })
+  });
 }
