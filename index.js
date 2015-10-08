0158423ff7eeefa52ae3fd688c6ee7bf8355abbf
Noah Zoschke <nzoschke@gmail.com>
2015-10-08 11:34:14 -0700

    refactor to cache URL on /tmp to avoid excessive CF DescribeStacks

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var aws = require('aws-sdk')
 var cf = new aws.CloudFormation()
-
+var fs = require('fs')
 var winston = require('winston')
 require('winston-papertrail').Papertrail
 
-exports.handler = function(event, context) {
+function logRecords(url, event, context) {
   // get app name from Kinesis record
   // e.g. arn:aws:kinesis:us-east-1:901416387788:stream/convox-Kinesis-L6MUKT1VH451 -> convox
   var appName = "unknown"
@@ -14,54 +14,72 @@ exports.handler = function(event, context) {
     appName = parts[1].split("-")[0]
   }
 
-  // get stack name from Lambda function name
-  // e.g. papertrail-PapertrailLogger-Y1HBW4I1TMB7 -> papertrail
-  var stackName = process.env.AWS_LAMBDA_FUNCTION_NAME.split("-")[0]
+  // connect to Papertrail
+  var tr = new winston.transports.Papertrail({
+    host: url.split(":")[0],
+    port: url.split(":")[1],
+    hostname: appName,
+    program: 'default',
+    showLevel: false,
+  })
 
-  cf.describeStacks({ StackName: stackName }, function(err, data) {
-    if (err) {
-      console.log(err, err.stack)
-      return context.fail(err)
+  var logger = new winston.Logger({ transports: [tr] })
+
+  // log every kinesis record to Papertrail
+  tr.on('connect', function() {
+    event.Records.forEach(function(record) {
+      var payload = new Buffer(record.kinesis.data, 'base64').toString('ascii')
+      logger.info(payload)
+    })
+
+    tr.close()
+
+    context.done(null)
+  })
+
+  tr.on('error', function(err) {
+    context.fail(err)
+  })
+}
+
+exports.handler = function(event, context) {
+  // check /tmp/url for cached Papertrail URL
+  fs.readFile('/tmp/url', function (err, url) {
+    if (!err) {
+      return logRecords(url.toString(), event, context)
     }
 
-    // get URL, host, port from stack parameters
-    var url = ""
+    // get stack name from Lambda function name
+    // e.g. papertrail-PapertrailLogger-Y1HBW4I1TMB7 -> papertrail
+    var stackName = process.env.AWS_LAMBDA_FUNCTION_NAME.split("-")[0]
 
-    var stack = data.Stacks[0]
-    for (var i = 0; i < stack.Parameters.length; i++) {
-      p = stack.Parameters[i]
-
-      if (p.ParameterKey == "Url") {
-        url = p.ParameterValue
-        break
+    cf.describeStacks({ StackName: stackName }, function(err, data) {
+      if (err) {
+        console.log(err, err.stack)
+        return context.fail(err)
       }
-    }
 
-    if (!url) {
-      return context.fail(Error("stack " + stackName + " is missing Url parameter"))
-    }
+      // get URL, host, port from stack parameters
+      var url = ""
 
-    // connect to Papertrail
-    var tr = new winston.transports.Papertrail({
-      host: url.split(":")[0],
-      port: url.split(":")[1],
-      hostname: appName,
-      program: 'default',
-      showLevel: false,
-    })
+      var stack = data.Stacks[0]
+      for (var i = 0; i < stack.Parameters.length; i++) {
+        p = stack.Parameters[i]
 
-    var logger = new winston.Logger({ transports: [tr] })
+        if (p.ParameterKey == "Url") {
+          url = p.ParameterValue
+          break
+        }
+      }
 
-    // log every kinesis record to Papertrail
-    tr.on('connect', function() {
-      event.Records.forEach(function(record) {
-        var payload = new Buffer(record.kinesis.data, 'base64').toString('ascii')
-        logger.info(payload)
-      })
+      if (!url) {
+        return context.fail(Error("stack " + stackName + " is missing Url parameter"))
+      }
 
-      tr.close()
+      fs.writeFileSync('/tmp/url', url)
 
-      context.done(null)
-    })
-  });
+      logRecords(url, event, context)
+    });
+
+  })
 }
